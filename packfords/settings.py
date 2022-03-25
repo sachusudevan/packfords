cfc6e845f2dfca1b5cfb2fd99fb0ca5ebfa4d0be
Sachu S <sachusudeve8690@gmail.com>
2022-03-25 11:51:57 +0530

    static url

--- packfords/settings.py
+++ packfords/settings.py
@@ -166,10 +166,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #Additional
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'assets')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
